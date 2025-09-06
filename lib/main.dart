6fcf9aaf44fa3ad0369cbf2813cc8c7ba4f9fa9a
Yiğit Samet Ölmez <ygtsmt@outlook.com>
2025-09-06 12:05:47 +0300

    Automated code generation for whale tracking Flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,62 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
 
 void main() {
-  runApp(MyApp());
+  runApp(WhaleTrackingApp());
 }
 
-class MyApp extends StatelessWidget {
+class WhaleTrackingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Whale Tracking',
-      home: WhaleTrackingScreen(),
+      home: WhaleListScreen(),
     );
   }
 }
 
-class WhaleTrackingScreen extends StatelessWidget {
-  final String jsonData = '''{"count":50,"limit":50,"status":"success","transactions":[{"id":"000000000000000000000000","whale_address":"0x000000000000000000000000000000000000dead","whale_name":"Null: 0x000...dEaD","whale_rank":6,"contract_address":"0x001208f7f53f78db2b32e1c68198d3e8f320aa23","token_name":"APD","token_symbol":"APD","token_type":"migrated","transaction_hash":"0x1263b9d4bb20fda332dcd398775941f1d942c301fd7aecebab99e7c53a47eba1","block_number":60106344,"timestamp":"2025-09-05T13:17:45Z","value":886252000000,"value_formatted":"0.000001","is_incoming":true,"token_market_cap":3859109927,"price_usd":"0.04158","created_at":"2025-09-05T13:17:48.570324484Z","image":"https://dd.dexscreener.com/ds-data/tokens/bsc/0x001208F7F53F78Db2b32E1c68198D3e8f320Aa23.png"}]}''';
+class WhaleListScreen extends StatelessWidget {
+  final List<Map<String, dynamic>> transactions = [
+    {
+      "whale_address": "0x8894e0a0c962cb723c1976a4421c95949be2d4e3",
+      "whale_name": "Binance: Hot Wallet 6",
+      "whale_rank": 51,
+      "contract_address": "0x2859e4544c4bb03966803b044a93563bd2d0dd4d",
+      "token_name": "SHIBA INU",
+      "token_symbol": "SHIB",
+      "transaction_hash": "0xa90cc043284d6e7a033f52de0cc2e7edef9c6fa094bf69925b775b3972472975",
+      "timestamp": "2025-09-06T08:56:01Z",
+      "value_formatted": "236777.920000"
+    },
+    {
+      "whale_address": "0x000000000000000000000000000000000000dead",
+      "whale_name": "Null: 0x000...dEaD",
+      "whale_rank": 6,
+      "contract_address": "0x206ec57d16fc60130ec6e3e0d748791f44a4e06a",
+      "token_name": "Unknown",
+      "token_symbol": "UNKNOWN",
+      "transaction_hash": "0x28ba9bbd80828f8bc694d79fe82e4bd20a069a9c8a2b68e67920dadbf3e2721d",
+      "timestamp": "2025-09-06T08:56:09Z",
+      "value_formatted": "776.192953"
+    },
+    {
+      "whale_address": "0x000000000000000000000000000000000000dead",
+      "whale_name": "Null: 0x000...dEaD",
+      "whale_rank": 6,
+      "contract_address": "0x001208f7f53f78db2b32e1c68198d3e8f320aa23",
+      "token_name": "Unknown",
+      "token_symbol": "UNKNOWN",
+      "transaction_hash": "0x3dc4cd9ffe0413cc18f151fde2d8b6386c97d11c0f69ee6007f03ef49b37ba60",
+      "timestamp": "2025-09-06T08:56:13Z",
+      "value_formatted": "0.000002"
+    },
+    // More entries would go here...
+  ];
 
   @override
   Widget build(BuildContext context) {
-    final Map<String, dynamic> data = json.decode(jsonData);
-    final List<dynamic> transactions = data['transactions'];
-
     return Scaffold(
       appBar: AppBar(
-        title: Text('Whale Transactions'),
+        title: Text('Whale Tracking'),
       ),
       body: ListView.builder(
         itemCount: transactions.length,
@@ -33,9 +64,22 @@ class WhaleTrackingScreen extends StatelessWidget {
           final transaction = transactions[index];
           return Card(
             margin: EdgeInsets.all(8.0),
-            child: ListTile(
-              title: Text('Address: ${transaction['whale_address']}'),
-              subtitle: Text('Amount: ${transaction['value_formatted']} USD\nTimestamp: ${transaction['timestamp']}'),
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Whale Address: ${transaction['whale_address']}', style: TextStyle(fontWeight: FontWeight.bold)),
+                  Text('Whale Name: ${transaction['whale_name']}'),
+                  Text('Whale Rank: ${transaction['whale_rank']}'),
+                  Text('Contract Address: ${transaction['contract_address']}'),
+                  Text('Token Name: ${transaction['token_name']}'),
+                  Text('Token Symbol: ${transaction['token_symbol']}'),
+                  Text('Transaction Hash: ${transaction['transaction_hash']}'),
+                  Text('Timestamp: ${transaction['timestamp']}'),
+                  Text('Value: ${transaction['value_formatted']}'),
+                ],
+              ),
             ),
           );
         },
